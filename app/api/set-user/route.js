8439bbd6772dfa9396d420e2aa8e6c7c2473d50d
Areej <areejatta.kcsf18@iba-suk.edu.pk>
2024-05-23 15:51:50 +0500

    update get route

--- app/api/set-user/route.js
+++ app/api/set-user/route.js
@@ -4,12 +4,11 @@ import { kv } from '@vercel/kv';
 export async function POST(request) {
     try {
         const data = await request.json();
-
-        console.log('Setting value in KV store:', data);
-        kv.lpush('users', data);
-        console.log('Value set successfully');
-
-        return NextResponse.json({ message: 'User Added Successfully!' });
+        kv.lpush('users', data);        
+        return NextResponse.json({ 
+            message: 'User Added Successfully!'
+            data: data 
+        });
     } catch (error) {
         console.error('Error storing user:', error);
         return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
