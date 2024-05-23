82fa8ba94846875d9bdd081c9599a21b1d96bafc
Areej <areejatta.kcsf18@iba-suk.edu.pk>
2024-05-23 15:48:12 +0500

    update get route

--- app/api/get-waitlist/route.js
+++ app/api/get-waitlist/route.js
@@ -2,10 +2,9 @@ import { NextResponse } from 'next/server';
 import { kv } from '@vercel/kv';
 
 export async function GET(req, ev) {
-    const { key } = await req.query.json()
     try {
-        const value = await kv.get(key);
-        return NextResponse.json({ key: value });
+        const value = await kv.get('users');
+        return NextResponse.json({ 'users': value });
     } catch (error) {
         console.error('Error retrieving value from KV store:', error);
         return NextResponse.json({ error: 'Error retrieving data' }, { status: 500 });
